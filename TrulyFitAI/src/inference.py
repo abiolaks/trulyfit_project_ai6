f5f89dd0ca38db6a0017cd31ab9a12fe156f81c1
Kingsley Lawani <36709697+abiolaks@users.noreply.github.com>
2025-10-29 23:18:38 +0100

    Dev clear (#25)
    * feat: add project Description to README
    
    * Feat: project file structure setup
    
    * rawdata:life_style.csv -version 1
    
    * add more code to the eda file
    
    * Process_Data version 1: replacespace in columns and turn them to lower case-data/processed
    
    * feat: carryout data preprocessing on the train data
    
    * Artifacts: Features for ML selected from train_set.csv- version 1
    
    * Artifacts: drop the categorical features; train only on the numerical first. version 2
    
    * Artifacts ml_data version 2: ml features prepared for ml
    
    * done with the preprocessing on the data
    
    * feat: Base Model training Using artifact data version 1
    
    * Artifact Test Data: Version 3
    
    * feat: Evaluate model script
    
    * feat: add evaluate and inference script
    
    * renam folder to truly fit ai
    
    * clear project struture
    
    * feat: pass data to model for inference
    
    * modify model training and evaluate.py

--- TrulyFitAI/src/inference.py
+++ TrulyFitAI/src/inference.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
 import os
+import subprocess
 from mlflow.models import infer_signature
 import pandas as pd
 
@@ -38,8 +39,44 @@ os.environ["MLFLOW_TRACKING_PASSWORD"] = os.getenv("MLFLOW_TRACKING_PASSWORD")
 logged_model = 'runs:/4bc4b3ab71bc43fc96adb9211e65cd58/RandomForest'
 
 # Load model as a PyFuncModel.
+mlflow.pyfunc.get_model_dependencies(logged_model)
 loaded_model = mlflow.pyfunc.load_model(logged_model)
 
+# get dependences for model
+deps_path = mlflow.pyfunc.get_model_dependencies(logged_model, format="pip")
+
+# Install the dependencies from the file
+subprocess.check_call(["pip", "install", "-r", deps_path])
+
+
+data = [73.67, 56.154249847892025, 23.51, 23.77596057025652, 1.847020496810099]
+
+batch_data_raw = [
+    [52.62, 41.043628751488114, 20.3, 21.99994536015182, 1.838274420372482],
+    [69.47, 49.409032892617006, 26.47, 28.877165837603265, 1.1872750827695409],
+    [81.51, 60.636696765010285, 20.38, 25.60827289288397, 0.941602257391731]
+]
+
+# The column names from the error message
+column_names = [
+    'weight_(kg)',
+    'lean_mass_kg',
+    'bmi',
+    'fat_percentage',
+    'protein_per_kg'
+]
+
+# Convert the data into a DataFrame with named columns
+#data_df = pd.DataFrame([data], columns=column_names)
+data_df = pd.DataFrame(batch_data_raw, columns=column_names)
+
+# Load your model (assuming it's already loaded in your script)
+# loaded_model = mlflow.pyfunc.load_model(...)
+
+# Predict with the correctly formatted DataFrame
+
+predictions = loaded_model.predict(data_df)
+
+print(np.round(predictions).astype(int))
+
 # Predict on your data.
-data = [73.67,56.154249847892025,23.51,23.77596057025652,1.847020496810099] # Code to load a data sample or samples
-Anloaded_model.predict(data)
